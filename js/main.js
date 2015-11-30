64072ca84817c13fe8ed17fd7d39beb372cdbec6
elishayer <eshayer@stanford.edu>
2015-11-29 22:24:12 -0800

    Convert Angular to Handlebars

--- js/main.js
+++ js/main.js
@@ -2,60 +2,88 @@
  * Eli Shayer
  * ----------
  * The main javascript file for my personal website.
+ * Uses the Handlebars templating library to generate html
  */
 
-// declare an angular module
-angular.module('es-app', [])
-	// declare an angular controller
-	.controller('es-control', function($scope, $sce) {
-		// initalize the active tab to about
-		$scope.activeTab = 'about';
-
-		// a general generateHtml function for all tabs
-		// takes care of Angular security concerns
-		$scope.generateHtml = function(tab) {
-			return $sce.trustAsHtml($scope.tabs[tab].generateHtml(tab));
-		}
-
-		// data on the tabs and a generating function for each tab
-		$scope.tabs = {
-			about : {
-				generateHtml : function(tab) {
-					return '<h3>' + tab + '</h3>';
+(function(window, document, undefined) {
+	// data for the tabs and corresponding data and templates
+	sections = {
+		intro:  {
+			template: [
+				'<div class="col-sm-4 col-xs-12">',
+					'<img src="{{ image.src }}" alt="{{ image.alt }}" id="{{ image.id }}"/>',
+				'</div>',
+				'<div class="col-sm-8 col-xs-12">',
+					'<h1>{{ content.title }}</h1>',
+					'<h3>{{ content.subtitle }}</h3>',
+					'{{#each content.text}}',
+					'<p>{{{ this }}}</p>',
+					'{{/each}}',
+				'</div>',
+			].join('\n'),
+			data    : {
+				image: { src: './images/eli-shayer-square.jpg', alt: 'Eli Shayer', id : 'portrait' },
+				content: {
+					title   : 'Eli Shayer',
+					subtitle: 'Stanford University class of 2018',
+					text    : [
+						'Hi! I am a sophomore at Stanford University from Anchorage, Alaska studying computer science and statistics. I am declared in Mathematical and Computational Science for my major, and am additionaly pursuing a minor in history.',
+						'I am particularly interested in web development and sports analytics. You can check out my personal history, projects I have worked on, and skills I possess in the tabs below.',
+						'Please feel free to <a href="mailto:contact@elishayer.com" target="_new">email me.'
+					]
 				}
-			},
-			projects : {
-				generateHtml : function(tab) {
-					return '<h3>' + tab + '</h3>';
+			}
+		},
+		tabs: {
+			template: [
+				'<div class="col-xs-12">',
+					'<ul class="nav nav-tabs nav-justified">',
+						'{{#each this}}',
+						'<li role="presentation">',
+							'<a href="#content">{{ name }} <i class="fa fa-{{icon}}"></i></a>',
+						'</li>',
+						'{{/each}}',
+					'<ul>',
+				'</div>'
+			].join('\n'),
+			data    : [
+				{
+					name: 'about',
+					icon: 'info-circle'
+				},
+				{
+					name: 'projects',
+					icon: 'tasks'
+				},
+				{
+					name: 'skills',
+					icon: 'laptop'
 				}
-			},
-			skills : {
-				generateHtml : function(tab) {
-					return '<h3>' + tab + '</h3>';
+			]
+		},
+		// TODO
 				}
 			}
 		}
 
-		$scope.socialLinks = [
-			{
-				name : "LinkedIn",
-				link : "https://www.linkedin.com/in/eli-shayer-62476b9b",
-				fa   : "fa-linkedin"
-			},
-			{
-				name : "GitHub",
-				link : "https://github.com/elishayer",
-				fa   : "fa-github"
-			},
-			{
-				name : "Twitter",
-				link : "https://twitter.com/elishayer",
-				fa   : "fa-twitter"
-			}
-		]
+	// For each section, compile the Handlebars template and render it with the
+	// associated data. Then place the resulting html in the DOM
+	for (section in sections) {
+		// get the template and data
+		var template = sections[section].template;
+		var data = sections[section].data;
 
-		// set the active tab based on the tab that was clicked
-		$scope.setActiveTab = function(tab) {
-			$scope.activeTab = tab;
+		// compile the template and pass in the data
+		var html = Handlebars.compile(template)(data);
+
+		$('#' + section).html(html);
+	}
+
+	// function to set the active tab
+	function setActiveTab(tab) {
+		for (tab in sections.tabs.data) {
+
 		}
-	});
+	}
+
+})(window, document);
