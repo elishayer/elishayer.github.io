b99bbc4f74760428e9722e21c9578e4fe87ebe73
elishayer <eshayer@stanford.edu>
2015-11-29 16:54:39 -0800

    Stub website without content

---
+++ js/main.js
@@ -0,0 +1,58 @@
+/* main.js
+ * Eli Shayer
+ * ----------
+ * The main javascript file for my personal website.
+ */
+
+// declare an angular module
+angular.module('es-app', [])
+	// declare an angular controller
+	.controller('es-control', function($scope, $sce) {
+		// initalize the active tab to about
+		$scope.activeTab = 'about';
+
+		// a general generateHtml function for all tabs
+		// takes care of Angular security concerns
+		$scope.generateHtml = function(tab) {
+			return $sce.trustAsHtml($scope.tabs[tab].generateHtml(tab));
+		}
+
+		// data on the tabs and a generating function for each tab
+		$scope.tabs = {
+			about : {
+				generateHtml : function(tab) {
+					return '<h3>' + tab + '</h3>';
+				}
+			},
+			projects : {
+				generateHtml : function(tab) {
+					return '<h3>' + tab + '</h3>';
+				}
+			},
+			skills : {
+				generateHtml : function(tab) {
+					return '<h3>' + tab + '</h3>';
+				}
+			}
+		}
+
+		$scope.socialLinks = [
+			{
+				name : "LinkedIn",
+				link : "https://www.linkedin.com/in/eli-shayer-62476b9b"
+			},
+			{
+				name : "GitHub",
+				link : "https://github.com/elishayer"
+			},
+			{
+				name : "Twitter",
+				link : "https://twitter.com/elishayer"
+			}
+		]
+
+		// set the active tab based on the tab that was clicked
+		$scope.setActiveTab = function(tab) {
+			$scope.activeTab = tab;
+		}
+	});
